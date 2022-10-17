c2719a6cba74e3d7c791f70928edf17ee27f7af6
niaaluf <niaaudita01@gmail.com>
2022-10-17 22:55:20 +0700

    Import Database

---
+++ src/main/java/websevice/c/toko_obatdb/TokoObatdbApplication.java
@@ -0,0 +1,13 @@
+package websevice.c.toko_obatdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoObatdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoObatdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/websevice/c/toko_obatdb/TokoObatdbApplicationTests.java
@@ -0,0 +1,13 @@
+package websevice.c.toko_obatdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoObatdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
